e9e1e408748425f443c51226083d438d032b6352
Martin <prsmtn@gmail.com>
2024-02-16 10:52:41 +0100

    f

--- Interfaces/AI_one/src/main.rs
+++ Interfaces/AI_one/src/main.rs
@@ -2,6 +2,7 @@
 use std::collections::HashMap;
 use std::thread::sleep;
 
+use bessie::bessie::State;
 use crab_rave_explorer::algorithm::{cheapest_border, move_to_cheapest_border};
 use oxagaudiotool::sound_config::{self, OxAgSoundConfig};
 use robotics_lib::event::events::Event;
@@ -19,44 +20,8 @@ use robotics_lib::{
     world::coordinates::Coordinate
 };
 fn main() {
-    // Setup the sound configs
-    // We suggest you to use small files as if you use too many big audio files the startup times may increase
-    let background_music = OxAgSoundConfig::new_looped_with_volume("audio/background.ogg", 2.0);
 
-    let mut events = HashMap::new();
-    // events.insert(Event::Ready, OxAgSoundConfig::new("assets/default/event/event_ready.ogg"));
-    // events.insert(Event::Terminated, OxAgSoundConfig::new("assets/default/event/event_terminated.ogg"));
-    // // events.insert(Event::EnergyRecharged(0), OxAgSoundConfig::new_with_volume("assets/default/event/event_energy_recharged.ogg", 0.1));
-    // events.insert(Event::AddedToBackpack(Content::None, 0), OxAgSoundConfig::new("assets/default/event/event_add_to_backpack.ogg"));
-    // events.insert(Event::RemovedFromBackpack(Content::None, 0), OxAgSoundConfig::new("assets/default/event/event_remove_from_backpack.ogg"));
-
-    let mut tiles = HashMap::new();
-    // tiles.insert(TileType::DeepWater, OxAgSoundConfig::new("assets/default/tile/tile_water.ogg"));
-    // tiles.insert(TileType::ShallowWater, OxAgSoundConfig::new("assets/default/tile/tile_water.ogg"));
-    // tiles.insert(TileType::Sand, OxAgSoundConfig::new("assets/default/tile/tile_sand.ogg"));
-    // tiles.insert(TileType::Grass, OxAgSoundConfig::new("assets/default/tile/tile_grass.ogg"));
-    // tiles.insert(TileType::Hill, OxAgSoundConfig::new("assets/default/tile/tile_grass.ogg"));
-    // tiles.insert(TileType::Mountain, OxAgSoundConfig::new("assets/default/tile/tile_mountain.ogg"));
-    // tiles.insert(TileType::Snow, OxAgSoundConfig::new("assets/default/tile/tile_snow.ogg"));
-    // tiles.insert(TileType::Lava, OxAgSoundConfig::new("assets/default/tile/tile_lava.ogg"));
-    // tiles.insert(TileType::Teleport(false), OxAgSoundConfig::new("assets/default/tile/tile_teleport.ogg"));
-    // tiles.insert(TileType::Street, OxAgSoundConfig::new("assets/default/tile/tile_street.ogg"));
-
-    let mut weather = HashMap::new();
-    // weather.insert(WeatherType::Rainy, OxAgSoundConfig::new("assets/default/weather/weather_rainy.ogg"));
-    // weather.insert(WeatherType::Foggy, OxAgSoundConfig::new("assets/default/weather/weather_foggy.ogg"));
-    // weather.insert(WeatherType::Sunny, OxAgSoundConfig::new("assets/default/weather/weather_sunny.ogg"));
-    // weather.insert(WeatherType::TrentinoSnow, OxAgSoundConfig::new("assets/default/weather/weather_winter.ogg"));
-    // weather.insert(WeatherType::TropicalMonsoon, OxAgSoundConfig::new("assets/default/weather/weather_tropical.ogg"));
-
-    // Create the audio tool
-    let audio = match OxAgAudioTool::new(events, tiles, weather) {
-        Ok(audio) => audio,
-        Err(e) => {
-            eprintln!("Failed to create OxAgAudioTool: {}", e);
-            std::process::exit(1);
-        }
-    };
+    let audio = get_audio_manager();
 
     let mut robot = Robottino {
         robot: Robot::new(),
@@ -66,16 +31,16 @@ fn main() {
 
     // world generator initialization
     let mut world_gen =
-        ghost_amazeing_island::world_generator::WorldGenerator::new(22, false, 1, 1.1);
+        ghost_amazeing_island::world_generator::WorldGenerator::new(505, false, 1, 1.1);
     // Runnable creation and start
     println!("Generating runnable (world + robot)...");
-    match robot.audio.play_audio(&background_music) {
-        Ok(_) => {},
-        Err(e) => {
-            eprintln!("Failed to play audio: {}", e);
-            std::process::exit(1);
-        }
-    }
+    // match robot.audio.play_audio(&background_music) {
+    //     Ok(_) => {},
+    //     Err(e) => {
+    //         eprintln!("Failed to play audio: {}", e);
+    //         std::process::exit(1);
+    //     }
+    // }
     let mut runner = Runner::new(Box::new(robot), &mut world_gen);
     println!("Runnable succesfully generated");
 
@@ -97,39 +62,14 @@ impl Runnable for Robottino {
         if self.robot.energy.get_energy_level() < 300 {
             self.robot.energy = rust_and_furious_dynamo::dynamo::Dynamo::update_energy();
         }
-        let ticks_until_weather = self.weather_tool.ticks_until_weather(robotics_lib::world::environmental_conditions::WeatherType::Rainy, 100000000000);
-        println!("Ticks until foggy: {:?}", ticks_until_weather);
-        // let audio1 = OxAgSoundConfig::new("src/audio/pluck_002.ogg");
-        // let audio2 = OxAgSoundConfig::new("src/audio/pluck_002.ogg");
-        // let audio3 = OxAgSoundConfig::new("src/audio/pluck_002.ogg");
-        // let audio4 = OxAgSoundConfig::new("src/audio/pluck_002.ogg");
-
-        // let mut my_map: HashMap<Event, OxAgSoundConfig> = HashMap::new();
-        // my_map.insert(Event::Ready, audio1);
-
-        // let mut my_tile_map: HashMap<TileType, OxAgSoundConfig> = HashMap::new();
-        // my_tile_map.insert(TileType::Grass, audio2);
-
-        // let mut my_weather_map: HashMap<WeatherType, OxAgSoundConfig> = HashMap::new();
-        // my_weather_map.insert(WeatherType::Sunny, audio3); // Removed .clone() here
+        weather_check(self);
         
-        // let audio_manager = OxAgAudioTool::new(my_map, my_tile_map, my_weather_map);
-
-        // match audio_manager {
-        //     Ok(mut audio_manager) => {
-        //         audio_manager.play_audio(&audio4);
-        //     },
-        //     Err(error) => {
-        //         println!("Failed to create audio manager: {}", error);
-        //     },
-        // }
-        //sleep
-        sleep(std::time::Duration::from_millis(30000));
-        DestroyZone.execute(world, self, Content::Tree(1));
-        WeatherPredictionTool::new();
-        // WeatherPredictionTool::ticks_until_weather(&WeatherPredictionTool::new(), required_weather, 1);
-
-        sleep(std::time::Duration::from_millis(300));
+        // sleep(std::time::Duration::from_millis(300));
+        // bessie::bessie::road_paving_machine(self, world, Direction::Up, State::MakeRoad);
+        DestroyZone.execute(world, self, Content::Rock(1));
+        let a = self.get_backpack();
+        print!("{:?}", a);
+        
         //print coordinate
         let coordinates = self.get_coordinate();
         println!("{:?}", coordinates);
@@ -191,4 +131,64 @@ impl Runnable for Robottino {
     fn get_backpack_mut(&mut self) -> &mut BackPack {
         &mut self.robot.backpack
     }
+}
+
+
+fn weather_check(robot: &Robottino) {
+    let ticks_until_weather = match robot.weather_tool.ticks_until_weather_change(100000000000) {
+        Ok(ticks) => ticks,
+        Err(e) => {
+            eprintln!("Failed to get ticks until weather change: {:?}", e);
+            return;
+        }
+    };
+    let predict = match robot.weather_tool.predict(ticks_until_weather) {
+        Ok(prediction) => prediction,
+        Err(e) => {
+            eprintln!("Failed to predict weather: {:?}", e);
+            return;
+        }
+    };
+    println!("Ticks until change: {:?}", ticks_until_weather);
+    println!("into: {:?}", predict);
+}
+
+fn get_audio_manager() -> OxAgAudioTool {
+    let background_music = OxAgSoundConfig::new_looped_with_volume("audio/background.ogg", 2.0);
+
+    let mut events = HashMap::new();
+    // events.insert(Event::Ready, OxAgSoundConfig::new("assets/default/event/event_ready.ogg"));
+    // events.insert(Event::Terminated, OxAgSoundConfig::new("assets/default/event/event_terminated.ogg"));
+    // // events.insert(Event::EnergyRecharged(0), OxAgSoundConfig::new_with_volume("assets/default/event/event_energy_recharged.ogg", 0.1));
+    // events.insert(Event::AddedToBackpack(Content::None, 0), OxAgSoundConfig::new("assets/default/event/event_add_to_backpack.ogg"));
+    // events.insert(Event::RemovedFromBackpack(Content::None, 0), OxAgSoundConfig::new("assets/default/event/event_remove_from_backpack.ogg"));
+
+    let mut tiles = HashMap::new();
+    // tiles.insert(TileType::DeepWater, OxAgSoundConfig::new("assets/default/tile/tile_water.ogg"));
+    // tiles.insert(TileType::ShallowWater, OxAgSoundConfig::new("assets/default/tile/tile_water.ogg"));
+    // tiles.insert(TileType::Sand, OxAgSoundConfig::new("assets/default/tile/tile_sand.ogg"));
+    // tiles.insert(TileType::Grass, OxAgSoundConfig::new("assets/default/tile/tile_grass.ogg"));
+    // tiles.insert(TileType::Hill, OxAgSoundConfig::new("assets/default/tile/tile_grass.ogg"));
+    // tiles.insert(TileType::Mountain, OxAgSoundConfig::new("assets/default/tile/tile_mountain.ogg"));
+    // tiles.insert(TileType::Snow, OxAgSoundConfig::new("assets/default/tile/tile_snow.ogg"));
+    // tiles.insert(TileType::Lava, OxAgSoundConfig::new("assets/default/tile/tile_lava.ogg"));
+    // tiles.insert(TileType::Teleport(false), OxAgSoundConfig::new("assets/default/tile/tile_teleport.ogg"));
+    // tiles.insert(TileType::Street, OxAgSoundConfig::new("assets/default/tile/tile_street.ogg"));
+
+    let mut weather = HashMap::new();
+    // weather.insert(WeatherType::Rainy, OxAgSoundConfig::new("assets/default/weather/weather_rainy.ogg"));
+    // weather.insert(WeatherType::Foggy, OxAgSoundConfig::new("assets/default/weather/weather_foggy.ogg"));
+    // weather.insert(WeatherType::Sunny, OxAgSoundConfig::new("assets/default/weather/weather_sunny.ogg"));
+    // weather.insert(WeatherType::TrentinoSnow, OxAgSoundConfig::new("assets/default/weather/weather_winter.ogg"));
+    // weather.insert(WeatherType::TropicalMonsoon, OxAgSoundConfig::new("assets/default/weather/weather_tropical.ogg"));
+
+    // Create the audio tool
+    let audio = match OxAgAudioTool::new(events, tiles, weather) {
+        Ok(audio) => audio,
+        Err(e) => {
+            eprintln!("Failed to create OxAgAudioTool: {}", e);
+            std::process::exit(1);
+        }
+    };
+    return audio;
 }
